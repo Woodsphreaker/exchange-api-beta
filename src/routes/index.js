ff5916a30bf6ef43ddda1837ba0ad6fd3b0a784e
Carlo Enrico Ferreira Joaquim <carlo.enrico@sp01.local>
2020-10-06 21:36:11 -0300

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const routes =  Router()
 
 const ExchangeController = require('../app/controllers/ExchangeController')
 
-routes.get('/cool', (req, res) => res.send(cool()))
+routes.get('/', (req, res) => res.send(cool()))
 routes.get('/listCurrency', ExchangeController.Index)
 
 module.exports = routes
