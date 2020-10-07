acb7810188bf081354aebc416e3b17402396c6e9
Carlo Enrico Ferreira Joaquim <carlo.enrico@sp01.local>
2020-10-06 21:18:12 -0300

    add currency package

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require('express')
+const cool = require('cool-ascii-faces');
 const routes =  Router()
 
 const ExchangeController = require('../app/controllers/ExchangeController')
 
+routes.get('/cool', (req, res) => res.send(cool()))
 routes.get('/listCurrency', ExchangeController.Index)
 
 module.exports = routes
